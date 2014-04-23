c12214ed4bd61c156f4f9cf9f59aab1e8364e0ba
Bart Jongejan <bart@cst.dk>
2014-04-23 09:45:02 +0200

    Added bracmat script to automatically edit the output from emcc to construct a ready-to-run single HTML page.

--- src/emscriptenHowToHTML.bat
+++ src/emscriptenHowToHTML.bat
@@ -1,2 +1,7 @@
 REM ..\..\emscripten\emcc json.c xml.c bracmat.c -o bracmat.html -s EXPORTED_FUNCTIONS="['_startProc','_Eval','_endProc','_oneShot']" -s IGNORED_FUNCTIONS="['_myputc']"
-..\..\emscripten\emcc json.c xml.c bracmat.c -o bracmatJS.html -s EXPORTED_FUNCTIONS="['_startProc','_Eval','_endProc','_oneShot']"
+ECHO starting script to generate bracmat as HTML page.
+call ..\..\emscripten\emcc json.c xml.c bracmat.c -o bracmatJS.html -s EXPORTED_FUNCTIONS="['_startProc','_Eval','_endProc','_oneShot']"
+ECHO now bracmat
+REM edit the next line so a natively compiled version of bracmat on your system is found.
+D:\projects\Bracmat\bin\bracmat.exe "get'\"editbracmatjs.bra\""
+ECHO bracmat done
