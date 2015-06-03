549eb5b7a1251320a41325023c9682660d8b7689
Bart Jongejan <bart@cst.dk>
2015-06-03 18:27:27 +0200

    Deleted files that have become unnecessary, more detiled file list in README.md

--- src/java/compileme.bat
+++
@@ -1,4 +0,0 @@
-javac ./dk/cst/*.java
-jar cfv "%CATALINA_HOME%/lib/bracmat.jar" dk/cst/bracmat.class
-javac -classpath "%CATALINA_HOME%/lib/bracmat.jar" ./bracmattest.java
-java -D"java.library.path=%CATALINA_HOME%/bin" -classpath "%CATALINA_HOME%/lib/bracmat.jar";. bracmattest
