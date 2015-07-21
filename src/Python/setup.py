1a968f4edc8995b5abaae995b601e87d4670a14e
Bart Jongejan <zgk261@alf.hum.ku.dk>
2015-07-21 21:12:09 +0200

    Added include path ../

--- src/Python/setup.py
+++ src/Python/setup.py
@@ -6,7 +6,7 @@ from distutils.extension import Extension
 
 sourcefiles = ['../bracmatso.c', '../xml.c', '../json.c', 'prythat.pyx']
 
-extensions = [Extension("prythat", sourcefiles, extra_compile_args=["-D_CRT_SECURE_NO_WARNINGS ","-DPYTHONINTERFACE"])]
+extensions = [Extension("prythat", sourcefiles, extra_compile_args=["-I../", "-D_CRT_SECURE_NO_WARNINGS","-DPYTHONINTERFACE"])]
 
 setup(
     ext_modules = cythonize(extensions)
