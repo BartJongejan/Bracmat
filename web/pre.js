67c14638350a47967935525d6276ac72391e4ad2
Bart Jongejan <bart.jongejan@gmail.com>
2022-05-07 09:57:09 +0200

    Added

---
+++ web/pre.js
@@ -0,0 +1,12 @@
+Module['print'] = (function () {
+    var element = document.getElementById('output');
+    if (element) element.value = ''; // clear browser cache
+    return function (text) {
+        if (arguments.length > 1) text = Array.prototype.slice.call(arguments).join(' ');
+        console.log(text);
+        if (element) {
+            element.value += text;
+            element.scrollTop = element.scrollHeight; // focus on bottom
+        }
+    };
+})();
