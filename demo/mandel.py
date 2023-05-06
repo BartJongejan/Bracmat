30e1472ead202a42008a6983742509741597c2bd
Bart Jongejan <bartj@hum.ku.dk>
2023-05-06 13:44:06 +0200

    Python implementation similar to C impl. Same output, different from bra

---
+++ demo/mandel.py
@@ -0,0 +1,95 @@
+''''
+for each pixel (Px, Py) on the screen do
+    x0 := scaled x coordinate of pixel (scaled to lie in the Mandelbrot X scale (-2.00, 0.47))
+    y0 := scaled y coordinate of pixel (scaled to lie in the Mandelbrot Y scale (-1.12, 1.12))
+    x := 0.0
+    y := 0.0
+    iteration := 0
+    max_iteration := 1000
+    while (x*x + y*y ≤ 2*2 AND iteration < max_iteration) do
+        xtemp := x*x - y*y + x0
+        y := 2*x*y + y0
+        x := xtemp
+        iteration := iteration + 1
+
+    color := palette[iteration]
+    plot(Px, Py, color)
+'''
+
+import time
+import math 
+
+iterations = 1000.0
+ypixels = 2000.0
+ratio = 1.0
+colfactor = 256.0 / iterations
+
+
+def compiledCalcule(v0,v1,v2,v3,v4,v5):
+    i = -1 + v3
+    T = [[0 for x in range(math.floor(v0))] for y in range(math.floor(v3))] 
+    while i >= 0.0:
+        j = -1 + v0
+        while j >= 0.0:
+            x0 = j * v1 + v2
+            y0 = i * v4 + v5
+            x = 0.0
+            y = 0.0
+            J = 1.0
+            while math.hypot(x, y) <= 2.0:
+                if J >= iterations:
+                    break
+                xtemp = x * x + -1.0 * y * y + x0
+                y = 2.0 * x * y + y0
+                x = xtemp
+                J = J + 1.0
+            if J < iterations:
+                T[math.floor(i)][math.floor(j)] = math.floor(J * colfactor)
+            else:
+                T[math.floor(i)][math.floor(j)] = 0.0
+
+            j = j + -1.0
+        i = i + -1.0
+    return T
+'''
+ 504.7275418999998 s
+'''
+
+'''
+def calculate(a0, a1):
+    return 1000
+ 1.390195500000118 s
+'''
+
+def doit():
+    X = -0.0452407411
+    Y = 0.9868162204352258
+    R = 2.7E-10
+    beginx = X + -1.0 * R
+    endx = X + R
+    beginy = Y + -1.0 * R
+    endy = Y + R
+    xpixels = math.floor(ratio * ypixels)
+    deltax = (endx + -1.0 * beginx) / (xpixels + -1.0)
+    deltay = (endy + -1.0 * beginy) / (ypixels + -1.0)
+
+    t0 = time.perf_counter()
+
+    T = compiledCalcule(xpixels, deltax, beginx, ypixels, deltay, beginy)
+
+    t1 = time.perf_counter()
+    print("Time:", t1 - t0 )
+
+    print("\nlooped")   
+    textfile = open("mandelPy.pgm", "w")
+
+    textfile.write("P3\n#Mandelbrot\n" + str(xpixels) + " " + str(math.floor(ypixels)) + "\n255\n")
+    for k in range(0,math.floor(xpixels)):
+        for m in range(0,math.floor(ypixels)):
+            arg = math.floor(T[k][m])
+            textfile.write( str(arg) + " " + str(arg) + " " + str(arg) + "\n")
+    textfile.close()
+
+    print("done")
+
+doit()
