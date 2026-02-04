5437fcd99083f86c706f2283a9e40e26a3064f37
Bart Jongejan <bart.jongejan@gmail.com>
2026-02-04 10:58:16 +0100

    Improved JavaScript and Web Assembly versions

--- WebAssembly/emscriptenHowToHTML.sh
+++ WebAssembly/emscriptenHowToHTML.sh
@@ -1,9 +1,23 @@
-#emcc (Emscripten gcc/clang-like replacement + linker emulating GNU ld) 3.1.9
-# Without WebAssembly:
-##emcc ../src/*.c --pre-js pre.js -s EXPORTED_RUNTIME_METHODS='["cwrap", "intArrayFromString"]' -s "EXPORTED_FUNCTIONS=['_main','_oneShot','_malloc']" -s "MODULARIZE=0" -O0 -o potuC.html -s "WASM=0"
- emcc ../src/*.c --pre-js pre.js -s EXPORTED_RUNTIME_METHODS='["stringToNewUTF8", "cwrap", "intArrayFromString","stackAlloc"]' -s "EXPORTED_FUNCTIONS=['_main','_oneShot','_malloc']" -s "MODULARIZE=0" -O0 -o potuC.html -s "WASM=0"
+# Set WASM=0 or WASM=1 in this script. Then execute it. 
+
+# Only JavaScript, without WebAssembly:
+WASM=0
+# Output: bracmatJS.html
+#    This file works fine by double clicking it in the file manager.
+
 # With WebAssembly
-# Note that you probably need to run from a web server. You can do `python3 -m http.server' and then load http://localhost:8000/bracmatJS.html in your browser.
-# See https://emscripten.org/docs/getting_started/FAQ.html#how-do-i-run-a-local-webserver-for-testing-why-does-my-program-stall-in-downloading-or-preparing
-#emcc ../src/*.c --pre-js pre.js -s EXPORTED_RUNTIME_METHODS='["stringToNewUTF8", "cwrap", "intArrayFromString","stackAlloc"]' -s "EXPORTED_FUNCTIONS=['_main','_oneShot','_malloc']" -s "MODULARIZE=0" -O0 -o potuC.html
-bracmat "get'\"edit-potuC.html.bra\""
+#WASM=1
+# Output: bracmatWASM.html and potuC.wasm
+#   These two files must be in the same directory under the webserver's document root.
+#   Note that you cannot activate Web Assembly if you just double click bracmatWASM.html in the file manager.
+#   You can do `python3 -m http.server' and then load http://localhost:8000/bracmatWASM.html in your browser.
+#   See https://emscripten.org/docs/getting_started/FAQ.html#how-do-i-run-a-local-webserver-for-testing-why-does-my-program-stall-in-downloading-or-preparing
+
+# emcc -v :
+# emcc (Emscripten gcc/clang-like replacement + linker emulating GNU ld) 4.0.4 (273f0216fede04f2445367765eaf2aabeeb60d84)
+# clang version 21.0.0git (https:/github.com/llvm/llvm-project 148111fdcf0e807fe74274b18fcf65c4cff45d63)
+
+emcc ../src/*.c --pre-js pre.js -s EXPORTED_RUNTIME_METHODS='["stringToNewUTF8", "cwrap", "intArrayFromString","stackAlloc"]' -s "EXPORTED_FUNCTIONS=['_main','_oneShot','_malloc']" -s "MODULARIZE=0" -O0 -o potuC.html -s "WASM=$WASM"
+bracmat "get'\"edit-potuC.html.bra\"" "$WASM"
+
+# last updated: 20260204
